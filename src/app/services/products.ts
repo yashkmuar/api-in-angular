bbea21b2cf58c38cb47a4eadd83233b6ff0d183b
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-22 12:02:32 +0530

    images loaded

---
+++ src/app/services/products.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Products {
+  constructor(private http:HttpClient){}
+
+  getProductList(){
+    const url = "https://dummyjson.com/products";
+    return this.http.get(url);
+  }
+}
